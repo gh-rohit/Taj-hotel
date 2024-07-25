c96d05694de57a07905f87f8db9c729192bc9a61
gh-rohit <rohitrajcse00@gmail.com>
2024-07-25 17:37:38 +0530

    third commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
    var tl=gsap.timeline()
    var video= document.querySelector("#video-container")
    var whiteImg=document.querySelector("#white-img")
    var goldenImg= document.querySelector("#golden-img")
+ 
    gsap.to("#nav",{
-    backgroundColor:"white",
+    backgroundColor:"#F6F5F5",
     height:"13.5vh",
-    
     scrollTrigger:{
         scroller:"body",
         trigger:"#page1",
@@ -60,18 +61,26 @@
   },"b")
 
 
-
+  gsap.to("#loader",{
+    y:-1000,
+    duration:2,
+    delay:1,
+   })
 
   tl.to("video",{
        width:"100%",
        height:"100vh",
-        duration: 1,
+        duration: 0.8,
+        delay:1.2,
+       
         ease: "power5.out",   
  }) 
  tl.from("#left-nav, #right-nav ",{
     y:100,
     opacity:0,
     duration: 0.8,
+    
+    
 
  })
 
@@ -121,7 +130,7 @@
  tl2.from("#page2 h2", {
    opacity: 0,
    scale: 0,
-   duration: 1,
+   duration: 0.5,
    scrollTrigger: {
      scroller: "body",
      trigger: "#page2",
@@ -146,16 +155,17 @@
   //   var eachText= dets.childNodes[1].innerHTML=clutter
       
     // })
+    
     gsap.from(".page3-pride h1 ",{
       y:150,
-      stagger:0.1,
-      duration:2,
+      stagger:0.9,
+      duration:1,
       scrollTrigger:{
         scroller:"body",
         trigger:"#page3",
         start:"25% 70%",
-        end:"10% 0%",
-        markers:true,
+        end:"26% 20%",
+        // markers:true,
         scrub: true
         
       }
@@ -169,10 +179,10 @@
                 duration: 0.5, // duration of the smooth scroll
                 easing: (t) => t, // linear easing function (default)
                 direction: 'vertical', // vertical scrolling (default)
-                smooth: true // enable smooth scrolling
+                smooth: true // 
             });
 
-            // Start the Lenis smooth scroll animation loop
+          
             function raf(time) {
                 lenis.raf(time);
                 requestAnimationFrame(raf);
@@ -183,19 +193,19 @@
           
         });
         
-  let lastScrollY = window.scrollY;
-const navbar = document.querySelector('#nav');
+//   let lastScrollY = window.scrollY;
+// const navbar = document.querySelector('#nav');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > lastScrollY) {
-        // Scrolling down
-        navbar.classList.add('hidden');
-    } else {
-        // Scrolling up
-        navbar.classList.remove('hidden');
-    }
-    lastScrollY = window.scrollY;
-});
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > lastScrollY) {
+//         // Scrolling down
+//         navbar.classList.add('hidden');
+//     } else {
+//         // Scrolling up
+//         navbar.classList.remove('hidden');
+//     }
+//     lastScrollY = window.scrollY;
+// });
 
 //  span tag of h1
 var roomTextElements = document.querySelectorAll('#room-text h1');
@@ -218,9 +228,9 @@ tl3.to("#room-text h1 span",{
 
 color:"#AD8B3A",
 stagger:0.3,
-duration:2,
+duration:1,
 onStart: () => {
-  console.log("ji")
+
 },
 scrollTrigger:{
    trigger:"#page4",
@@ -320,8 +330,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const text2=container.querySelector(".text-with-icon p")
 
     gsap.fromTo(container, {
-      opacity: 0,
-      y: 50
+      opacity: 0.1,
+      y: 30
     }, {
       opacity: 1,
       y: 0,
@@ -336,23 +346,192 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     container.addEventListener("mouseenter", () => {
-      gsap.to(image, { duration: 0.5, x: 300 }); // Move image into view
+      gsap.to(image, { duration: 0.5, x: 300 }); 
       gsap.to(text, { duration: 0.5, x: 300 }); 
       gsap.to(text2, { duration: 0.5, x: 300 }); 
       gsap.to(text,{color:"  #fff  "}) 
       gsap.to(text2,{color:"#fff"})
-      gsap.to(container,{backgroundColor:"#D4B467"})// Move text to the right
+      gsap.to(container,{backgroundColor:"#D4B467"})
     });
 
     container.addEventListener("mouseleave", () => {
-      gsap.to(image, { duration: 0.5, x: -300 }); // Move image out of view
+      gsap.to(image, { duration: 0.5, x: -300 }); 
       gsap.to(text, { duration: 0.5, x: 0 }); 
       gsap.to(text2, { duration: 0.5, x: 0 }); 
       gsap.to(text,{color:"#AD8B3A"}) 
       gsap.to(text2,{color:"#AD8B3A"})
-      gsap.to(container,{backgroundColor:"#fff"})  // Move text back to its original position
+      gsap.to(container,{backgroundColor:"#fff"})  
     });
   });
 });
 
 
+let tl5 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#page6",
+    scroller: "body",
+    start: "25% 50%",
+    end: "top 80%",
+    markers: true,
+
+
+  }
+});
+
+tl5.to("#page6-line", {
+  duration: 0.7,
+  width: "75px",
+  ease: "power2.inOut"
+});
+tl5.to('.page6-img-overlay',{
+  y:"-600",
+  ease: "power1.inOut",
+  duration:0.7,
+
+
+})
+
+document.addEventListener('mousemove', (event) => {
+  gsap.to('#follower', {
+      duration: 0.5,
+      x: event.clientX + 0,  
+      y: event.clientY + 10,  
+      ease: 'power1.out'
+  });
+});
+
+
+var diningText = document.querySelectorAll('#dining-text h1');
+
+diningText.forEach(function(elem) {
+    var split = elem.textContent.split("");
+    var clutter = "";
+    
+    split.forEach(function(char) {
+        clutter += `<span>${char}</span>`;
+    });
+    
+    elem.innerHTML = clutter;
+   
+});
+var tl10=gsap.timeline()
+tl10.to("#dining-text h1 span",{
+  // y:-50,
+  opacity:1,
+
+color:"#E3E3C4",
+stagger:0.3,
+duration:1,
+onStart: () => {
+
+},
+scrollTrigger:{
+   trigger:"#page7",
+   scroller:"body",
+  //  markers:true,
+   start:"-5% 50%",
+   end:"20% 34%",
+  //  pin: true,
+   scrub:2
+}
+})
+ tl10.from('#dining-text p',{
+  y:100,
+  opacity:0,
+  scrollTrigger:{
+    trigger:"#page7",
+    scroller:"body",
+   //  markers:true,
+    start:"-5% 50%",
+    end:"20% 34%",
+   //  pin: true,
+    scrub:2
+
+  }
+ })
+ tl10.from('.page7-left',{
+  y:100,
+  opacity:0,
+  scrollTrigger:{
+    trigger:"#page7-content",
+    scroller:"body",
+   //  markers:true,
+    start:"-5% 50%",
+    end:"20% 34%",
+   //  pin: true,
+    scrub:2
+
+  }
+ })
+ tl10.from('#page7-center',{
+  y:200,
+  opacity:0,
+  scrollTrigger:{
+    trigger:"#page7-content",
+    scroller:"body",
+   //  markers:true,
+    start:"0% 50%",
+    end:"20% 34%",
+   //  pin: true,
+    scrub:2
+
+  }
+ })
+ 
+
+ let next = document.querySelector('.next')
+ let prev = document.querySelector('.prev')
+ 
+ next.addEventListener('click', function(){
+     let items = document.querySelectorAll('.item')
+     document.querySelector('.slide').appendChild(items[0])
+ })
+ 
+ prev.addEventListener('click', function(){
+     let items = document.querySelectorAll('.item')
+     document.querySelector('.slide').prepend(items[items.length - 1]) 
+ })
+
+  document.querySelectorAll('.review-card').forEach(function(elem){
+    gsap.from(elem, {
+      duration: 1,
+      opacity: 0,
+      y: 50,
+      stagger: 0.3,
+      ease: "power2.out",
+      scrollTrigger:{
+        trigger:".reviews-section",
+        sroller:"body",
+        start:"top 50%",
+        end:"top 20%",
+        // markers:true
+      }
+    });
+
+  })
+  document.addEventListener("DOMContentLoaded", function() {
+    gsap.from(".brand-logo", {
+      duration: 1,
+      opacity: 0,
+      y: 20,
+      stagger: 0.3,
+      ease: "power2.out"
+    });
+  });
+  
+  
+
+   gsap.from(".page8-explore h1",{
+    y:100,
+    duration:0.7,
+    opacity:0.5,
+    rotate:"-10deg",
+    scrollTrigger:{
+      trigger:".page8-explore",
+      scroller:"body",
+      scrub:0.2,
+      start:"50% 85%",
+      end:" 10% 50%",
+      markers:true
+    }
+   })
